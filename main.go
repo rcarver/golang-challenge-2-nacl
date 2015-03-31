30d09ce6571c93bfaea1a0af97e003ce84bdd764
Ryan Carver <ryan@ryancarver.com>
2015-03-31 11:53:11 -0400

    formatted main and main_test

--- main.go
+++ main.go
@@ -1,65 +1,65 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "io"
-        "log"
-        "net"
-        "os"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
 )
 
 // NewSecureReader instantiates a new SecureReader
 func NewSecureReader(r io.Reader, priv, pub *[32]byte) io.Reader {
-        return nil
+	return nil
 }
 
 // NewSecureWriter instantiates a new SecureWriter
 func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
-        return nil
+	return nil
 }
 
 // Dial generates a private/public key pair,
 // connects to the server, perform the handshake
 // and return a reader/writer.
 func Dial(addr string) (io.ReadWriteCloser, error) {
-        return nil, nil
+	return nil, nil
 }
 
 // Serve starts a secure echo server on the given listener.
 func Serve(l net.Listener) error {
-        return nil
+	return nil
 }
 
 func main() {
-        port := flag.Int("l", 0, "Listen mode. Specify port")
-        flag.Parse()
+	port := flag.Int("l", 0, "Listen mode. Specify port")
+	flag.Parse()
 
-        // Server mode
-        if *port != 0 {
-                l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-                if err != nil {
-                        log.Fatal(err)
-                }
-                defer l.Close()
-                log.Fatal(Serve(l))
-        }
+	// Server mode
+	if *port != 0 {
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer l.Close()
+		log.Fatal(Serve(l))
+	}
 
-        // Client mode
-        if len(os.Args) != 3 {
-                log.Fatalf("Usage: %s <port> <message>", os.Args[0])
-        }
-        conn, err := Dial("localhost:" + os.Args[1])
-        if err != nil {
-                log.Fatal(err)
-        }
-        if _, err := conn.Write([]byte(os.Args[2])); err != nil {
-                log.Fatal(err)
-        }
-        buf := make([]byte, len(os.Args[2]))
-        n, err := conn.Read(buf)
-        if err != nil {
-                log.Fatal(err)
-        }
-        fmt.Printf("%s\n", buf[:n])
+	// Client mode
+	if len(os.Args) != 3 {
+		log.Fatalf("Usage: %s <port> <message>", os.Args[0])
+	}
+	conn, err := Dial("localhost:" + os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := conn.Write([]byte(os.Args[2])); err != nil {
+		log.Fatal(err)
+	}
+	buf := make([]byte, len(os.Args[2]))
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", buf[:n])
 }
