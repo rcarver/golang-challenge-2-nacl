18dd5e22df02cf2ce58609dab3f270a29893fbfc
Ryan Carver <ryan@ryancarver.com>
2015-04-02 14:17:20 -0400

    extract server and client classes

--- main.go
+++ main.go
@@ -19,90 +19,34 @@ func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
 	return &SecureWriter{w, priv, pub}
 }
 
-type RW struct {
-	r io.Reader
-	w io.Writer
-	c net.Conn
-}
-
-func (rw *RW) Read(buf []byte) (int, error) {
-	return rw.r.Read(buf)
-}
-
-func (rw *RW) Write(buf []byte) (int, error) {
-	return rw.w.Write(buf)
-}
-
-func (rw *RW) Close() error {
-	return rw.c.Close()
-}
-
 // Dial generates a private/public key pair,
 // connects to the server, perform the handshake
 // and return a reader/writer.
 func Dial(addr string) (io.ReadWriteCloser, error) {
-	logger := log.New(os.Stderr, "client: ", log.Lshortfile)
+	// Generate private key.
+	priv := &[32]byte{'p', 'r', 'i', 'v', '!'}
 
+	// Connect on the network.
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
 
-	// Generate private key.
-	priv := &[32]byte{'p', 'r', 'i', 'v', '!'}
-
-	// Receive private key from the server.
-	logger.Printf("Receiving public key...\n")
-	pub := &[32]byte{}
-	if _, err = conn.Read(pub[:]); err != nil {
+	// Initialize the client, perform key handshake and return
+	// a piped connection to the server.
+	c := NewClient(priv)
+	if err := c.ReadPublicKey(conn); err != nil {
 		return nil, err
 	}
-	logger.Printf("Received public key: %v\n", pub)
-
-	// Set up secure read and write.
-	r := NewSecureReader(conn, priv, pub)
-	w := NewSecureWriter(conn, priv, pub)
-	return &RW{r, w, conn}, nil
+	return c.SecureConn(conn), nil
 
 }
 
 // Serve starts a secure echo server on the given listener.
 func Serve(l net.Listener) error {
-	logger := log.New(os.Stderr, "server: ", log.Lshortfile)
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			return err
-		}
-		defer conn.Close()
-
-		// Generate public key.
-		pub := &[32]byte{'p', 'u', 'b', '!'}
-
-		// Send public key to the client.
-		logger.Printf("Sending public key...\n")
-		if _, err := conn.Write(pub[:]); err != nil {
-			return err
-		}
-
-		// Read input from the client.
-		logger.Printf("Reading...\n")
-		buf := make([]byte, 2048)
-		c, err := conn.Read(buf)
-		if err != nil {
-			return err
-		}
-		logger.Printf("Read %d bytes\n", c)
-
-		// Echo it back unmodified.
-		logger.Printf("Writing...\n")
-		c, err = conn.Write(buf[:c])
-		if err != nil {
-			return err
-		}
-		logger.Printf("Wrote %d bytes\n", c)
-	}
-	return nil
+	// Generate public key.
+	pub := &[32]byte{'p', 'u', 'b', '!'}
+	return NewServer(pub).Serve(l)
 }
 
 func main() {
---
+++ net.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+// Server is the encrypted echo server.
+type Server struct {
+	pub    *[32]byte
+	logger *log.Logger
+}
+
+// NewServer initializes a new Server with the public key.
+func NewServer(pub *[32]byte) *Server {
+	logger := log.New(os.Stderr, "server: ", log.Lshortfile)
+	return &Server{pub, logger}
+}
+
+// Serve starts an infinite loop waiting for client connections.
+func (s *Server) Serve(l net.Listener) error {
+	conns := s.acceptClients(l)
+	for {
+		go func() {
+			conn := <-conns
+			defer conn.Close()
+			if err := s.handleClient(conn); err != nil {
+				s.log("Error handling client: %s", err)
+			}
+		}()
+	}
+}
+
+// handleClient is the main handler for client/server behavior.
+func (s *Server) handleClient(conn net.Conn) error {
+	// Send public key to the client.
+	s.log("Sending public key...\n")
+	if _, err := conn.Write(s.pub[:]); err != nil {
+		return err
+	}
+
+	// Read input from the client.
+	s.log("Reading...\n")
+	buf := make([]byte, 2048)
+	c, err := conn.Read(buf)
+	if err != nil {
+		return err
+	}
+	s.log("Read %d bytes\n", c)
+
+	// Echo it back unmodified.
+	s.log("Writing...\n")
+	c, err = conn.Write(buf[:c])
+	if err != nil {
+		return err
+	}
+	s.log("Wrote %d bytes\n", c)
+
+	return nil
+}
+
+// acceptClients pushes new client connections to a channel.
+func (s *Server) acceptClients(l net.Listener) chan net.Conn {
+	ch := make(chan net.Conn)
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				s.log("Failed to accept client: %s", err)
+				return
+			}
+			ch <- conn
+		}
+	}()
+	return ch
+}
+
+func (s *Server) log(str string, v ...interface{}) {
+	s.logger.Printf(str, v...)
+}
+
+// Client is the secure echo client.
+type Client struct {
+	priv   *[32]byte
+	pub    *[32]byte
+	logger *log.Logger
+}
+
+// NewClient initializes a Client with the private key. Its public key
+// will be retrieved from the server.
+func NewClient(priv *[32]byte) *Client {
+	logger := log.New(os.Stderr, "client: ", log.Lshortfile)
+	return &Client{priv: priv, logger: logger}
+}
+
+// GetPublicKey retrieves the public key from the server.
+func (c *Client) ReadPublicKey(conn net.Conn) error {
+	// Receive private key from the server.
+	c.log("Receiving public key...\n")
+	c.pub = &[32]byte{}
+	if _, err := conn.Read(c.pub[:]); err != nil {
+		return err
+	}
+	c.log("Received public key: %v\n", c.pub)
+	return nil
+}
+
+// SecureConn returns a ReadWriteCloser to communicate with the server.
+// Requires that a public key has been provided, probably by reading it via
+// ReadPublicKey.
+func (c *Client) SecureConn(conn net.Conn) io.ReadWriteCloser {
+	r := NewSecureReader(conn, c.priv, c.pub)
+	w := NewSecureWriter(conn, c.priv, c.pub)
+	return &connIO{r, w, conn}
+}
+
+type connIO struct {
+	r io.Reader
+	w io.Writer
+	c net.Conn
+}
+
+func (rw *connIO) Read(buf []byte) (int, error) {
+	return rw.r.Read(buf)
+}
+
+func (rw *connIO) Write(buf []byte) (int, error) {
+	return rw.w.Write(buf)
+}
+
+func (rw *connIO) Close() error {
+	return rw.c.Close()
+}
+
+func (c *Client) log(str string, v ...interface{}) {
+	c.logger.Printf(str, v...)
+}
