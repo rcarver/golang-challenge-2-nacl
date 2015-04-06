c74956886ec08716cf8548fa1b19300791be49c3
Ryan Carver <ryan@ryancarver.com>
2015-04-05 20:27:56 -0700

    hooray key exchange

--- dh/main.go
+++ dh/main.go
@@ -1,16 +1,14 @@
-// This program attempts to do a Diffie-Hellman key exchange using NaCL.
+// This program performs a Diffie-Hellman key exchange using NaCL.
 package main
 
 import (
 	"crypto/rand"
 	"fmt"
 
-	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/nacl/box"
 )
 
 func main() {
-
 	fmt.Printf("Key pairs\n")
 
 	aPub, aPriv, _ := box.GenerateKey(rand.Reader)
@@ -25,32 +23,17 @@ func main() {
 	fmt.Printf("Shared keys\n")
 
 	aShare := &[32]byte{}
-	Compute(aShare, aPub, aPriv)
+	box.Precompute(aShare, aPub, bPriv)
 
 	bShare := &[32]byte{}
-	Compute(bShare, bPub, bPriv)
+	box.Precompute(bShare, bPub, aPriv)
 
 	fmt.Printf("a share %v\n", aShare)
 	fmt.Printf("b share %v\n", bShare)
 
 	aComm := &[32]byte{}
-	Compute(aComm, aPriv, bShare)
+	box.Precompute(aComm, aShare, bPriv)
 
 	bComm := &[32]byte{}
-	Compute(bComm, bPriv, aShare)
-
-	fmt.Printf("\n")
-	fmt.Printf("Common keys\n")
-
-	fmt.Printf("a comm %v\n", aComm)
-	fmt.Printf("b comm %v\n", bComm)
-
-}
-
-var zeros [16]byte
-
-// This is box.Precompute for playing with.
-func Compute(sharedKey, peersPublicKey, privateKey *[32]byte) {
-	curve25519.ScalarMult(sharedKey, privateKey, peersPublicKey)
-	//salsa.HSalsa20(sharedKey, &zeros, sharedKey, &salsa.Sigma)
+	box.Precompute(bComm, bShare, aPriv)
 }
